e84dbd31c255c81770722ff96a28c7d11beb23d8
zulfahmidev <developer.zulfahmi@gmail.com>
2023-07-20 10:19:24 +0700

    memperbaiki response

--- app/Models/Proposal.php
+++ app/Models/Proposal.php
@@ -15,6 +15,10 @@ class Proposal extends Model
 
     public function getDetail() {
         $proposal = Proposal::find($this->id);
+        $proposal->project = Project::find($this->project_id)->getDetail();
+        $proposal->lecture = User::find($this->lecture_id)->getDetail();
+        unset($proposal->project_id);
+        unset($proposal->lecture_id);
         $members = [];
         $attachments = [];
         foreach (ProposalMember::where('proposal_id', $this->id)->get() as $member) {
